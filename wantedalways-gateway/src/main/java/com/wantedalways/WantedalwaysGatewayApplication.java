620d885e0c195f42bde10a7e8363909dfecce200
Wantedalways <18366112991@163.com>
2022-12-16 16:16:16 +0800

    项目初始化-2.创建网关和系统基础模块

---
+++ wantedalways-gateway/src/main/java/com/wantedalways/WantedalwaysGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.wantedalways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedalwaysGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantedalwaysGatewayApplication.class, args);
+    }
+
+}
---
+++ wantedalways-system/src/main/java/com/wantedalways/WantedalwaysSystemApplication.java
@@ -0,0 +1,13 @@
+package com.wantedalways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedalwaysSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantedalwaysSystemApplication.class, args);
+    }
+
+}
